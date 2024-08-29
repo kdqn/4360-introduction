4ff8fcd81e2b7c62066e1e3652a077d778d356ed
CaydenLaptop <cayden.odegard@gmail.com>
2024-08-28 23:07:06 -0500

    added more info

--- index.py
+++ index.py
@@ -11,7 +11,7 @@ if question == "/name":
 elif question == "/why":
     print("I chose CS because my dad also has a CS degree. It is familiar to me, and I enjoy figuring out how computers work. I've enjoyed messing with computers from a very young age.")
 elif question == "/what":
-    print("I am not positive exactly what I want to do with my degree, but I would like a job that has some sort of graphic element as I enjoy the visual aspect of coding as well as the background programs.")
+    print("I am not positive exactly what I want to do with my degree, but I would like a job that has some sort of graphic element as I enjoy the visual aspect of coding as well as the background programs. I also may pursue a GIS minor as i'm one course away from that in addition to my degree")
 elif question == "/fact":
     print("I'm a junior in college and I'm only 17!")    
 else:
